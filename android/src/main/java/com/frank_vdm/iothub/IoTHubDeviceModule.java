d30c02050d6d2a2f858105754187e13fd7be306b
Francois van der Merwe <francois@asgard.works>
2019-07-26 14:21:23 +0200

    Update IoTHubDeviceModule.java

--- android/src/main/java/com/frank_vdm/iothub/IoTHubDeviceModule.java
+++ android/src/main/java/com/frank_vdm/iothub/IoTHubDeviceModule.java
@@ -366,7 +366,7 @@ public class IoTHubDeviceModule extends ReactContextBaseJavaModule {
                 try {
                     client.open();
                 } catch (IOException ioException) {
-                    if (StringUtils.containsIgnoreCase(ExceptionUtils.getRootCauseMessage(e), "TransportException: Timed out waiting to connect to service") && _shouldRetry) {
+                    if (StringUtils.containsIgnoreCase(ExceptionUtils.getRootCauseMessage(ioException), "TransportException: Timed out waiting to connect to service") && _shouldRetry) {
                         try {
                             Thread.sleep(2000);
                             _shouldRetry = false;
